c63a78e0b1979729de17f6951310d63550599027
marvin-j97 <marvin.janke.97@gmail.com>
2023-10-23 21:58:09 +0200

    fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import unoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/solid-2048",
   integrations: [
     solidJs(),
     unoCSS({ injectReset: true })
