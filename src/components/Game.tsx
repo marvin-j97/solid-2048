f1e16b3deae3671110d217762085c846bd7be59c
marvin-j97 <marvin.janke.97@gmail.com>
2023-10-23 21:49:39 +0200

    use objects for matrix

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,34 +1,36 @@
-import { createSignal, For, Show, type JSXElement, onMount, onCleanup } from "solid-js";
+import { createEffect, createSignal, For, Show, type JSXElement, onMount, onCleanup } from "solid-js";
 import { TransitionGroup } from "solid-transition-group";
 
 type Tile = {
   value: number;
 }
 
-function spawnRandomTile(arr: number[], count = 1): number[] {
+const box = (x: number): Tile => ({ value: x })
+
+function spawnRandomTile(arr: Tile[], count = 1): Tile[] {
   const copy = [...arr];
 
   let created = 0;
 
   while (created < count) {
-    if (copy.every(x => x > 0)) {
+    if (copy.every(x => x.value > 0)) {
       return copy;
     }
 
     const idx = Math.floor(Math.random() * copy.length);
-    if (copy[idx]! > 0) {
+    if (copy[idx]!.value > 0) {
       continue;
     }
 
-    copy[idx] = Math.random() > 0.5 ? 2 : 4;
+    copy[idx] = box(Math.random() > 0.5 ? 2 : 4);
     created++;
   }
 
   return copy;
 }
 
-function createInitialField(): number[] {
-  const field = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+function createInitialField(): Tile[] {
+  const field = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map(_ => box(0));
   return spawnRandomTile(field, 2);
 }
 
@@ -53,7 +55,7 @@ function shallowCopyMatrix<T>(matrix: Matrix<T>): Matrix<T> {
   return copiedMatrix as Matrix<T>;
 }
 
-function rotateMatrixClockwise(matrix: Matrix<number>): Matrix<number> {
+function rotateMatrixClockwise<T>(matrix: Matrix<T>): Matrix<T> {
   const copy = shallowCopyMatrix(matrix);
 
   matrix.forEach((row, i) => {
@@ -62,10 +64,10 @@ function rotateMatrixClockwise(matrix: Matrix<number>): Matrix<number> {
     });
   });
 
-  return copy as Matrix<number>;
+  return copy as Matrix<T>;
 }
 
-function rotateMatrixCounterClockwise(matrix: Matrix<number>): Matrix<number> {
+function rotateMatrixCounterClockwise<T>(matrix: Matrix<T>): Matrix<T> {
   const copy = shallowCopyMatrix(matrix);
 
   matrix.forEach((row, i) => {
@@ -74,11 +76,10 @@ function rotateMatrixCounterClockwise(matrix: Matrix<number>): Matrix<number> {
     });
   });
 
-
-  return copy as Matrix<number>;
+  return copy as Matrix<T>;
 }
 
-function shiftMatrixRight(input: Matrix<number>): Matrix<number> {
+function shiftMatrixRight(input: Matrix<Tile>, addScore: (x: number) => void): Matrix<Tile> {
   const matrix = shallowCopyMatrix(input);
 
   for (let i = 0; i < 4; i++) {
@@ -89,7 +90,7 @@ function shiftMatrixRight(input: Matrix<number>): Matrix<number> {
 
     for (let j = 2; j >= 0; j--) {
       const currentTile = row[j]!;
-      if (currentTile === 0) {
+      if (currentTile.value === 0) {
         continue;
       }
 
@@ -101,13 +102,14 @@ function shiftMatrixRight(input: Matrix<number>): Matrix<number> {
 
         const nextTile = row[indexToMergeWith]!;
 
-        if (canMerge && currentTile === nextTile) {
-          row[j] = 0;
-          row[indexToMergeWith] *= 2;
+        if (canMerge && currentTile.value === nextTile.value) {
+          row[j] = box(0);
+          row[indexToMergeWith] = box(currentTile.value * 2);
           canMerge = false;
+          addScore(currentTile.value * 2);
           break;
         }
-        if (nextTile > 0) {
+        if (nextTile.value > 0) {
           break;
         }
 
@@ -118,15 +120,20 @@ function shiftMatrixRight(input: Matrix<number>): Matrix<number> {
     // Move all to right as far as possible
     for (let j = 2; j >= 0; j--) {
       const currentTile = row[j]!;
-      if (currentTile === 0) {
+      if (currentTile.value === 0) {
         continue;
       }
 
       let indexToMoveTo = j;
 
       while (true) {
-        const nextTile = row[indexToMoveTo + 1]!;
-        if (nextTile > 0) {
+        const nextTile = row[indexToMoveTo + 1];
+
+        if (!nextTile) {
+          break;
+        }
+
+        if (nextTile.value > 0) {
           break;
         }
 
@@ -134,7 +141,7 @@ function shiftMatrixRight(input: Matrix<number>): Matrix<number> {
           break;
         }
 
-        if (nextTile === 0) {
+        if (nextTile.value === 0) {
           indexToMoveTo += 1;
         }
         else {
@@ -142,56 +149,92 @@ function shiftMatrixRight(input: Matrix<number>): Matrix<number> {
         }
       }
 
-      row[j] = 0;
+      row[j] = box(0);
       row[indexToMoveTo] = currentTile;
-
     }
   }
 
   return matrix;
 }
 
+function fieldsAreEqual(a: Tile[], b: Tile[]): boolean {
+  if (a.length !== b.length) {
+    return false;
+  }
+  return a.every((a, i) => b[i]!.value === a.value);
+}
+
 export default function Game(): JSXElement {
   const [field, setField] = createSignal(createInitialField());
+  const [hasWon, setWin] = createSignal(false);
+  const [score, setScore] = createSignal(0);
+
+  function addScore(points: number): void {
+    setScore(x => x + points);
+  }
 
   function reset(): void {
     setField(createInitialField());
+    setWin(false);
+    setScore(0);
   }
+
+  createEffect(() => {
+    if (field().some(x => x.value === 2_048)) {
+      setWin(true);
+    }
+  });
 
   onMount(() => {
     const handler = (ev: KeyboardEvent) => {
+      if (hasWon()) {
+        return;
+      }
 
-      // TODO: if move changed nothing, don't spawn tile
+      let deltaScore = 0;
+
+      function onMove(nextField: Tile[]) {
+        if (fieldsAreEqual(field(), nextField)) {
+          return;
+        }
+
+        setField(spawnRandomTile(nextField));
+        addScore(deltaScore);
+      }
 
       switch (ev.key) {
         case "ArrowUp": {
           const matrix = buildMatrix(field());
           const rotatedMatrixView = rotateMatrixClockwise(matrix);
-          const shifted = shiftMatrixRight(rotatedMatrixView);
+          const shifted = shiftMatrixRight(rotatedMatrixView, (n) => deltaScore += n);
           const rotatedBack = rotateMatrixCounterClockwise(shifted);
-          setField(spawnRandomTile(matrixToField(rotatedBack)));
+          const nextField = matrixToField(rotatedBack);
+          onMove(nextField);
           break;
         }
         case "ArrowDown": {
           const matrix = buildMatrix(field());
           const rotatedMatrixView = rotateMatrixClockwise(rotateMatrixClockwise(rotateMatrixClockwise(matrix)));
-          const shifted = shiftMatrixRight(rotatedMatrixView);
+          const shifted = shiftMatrixRight(rotatedMatrixView, (n) => deltaScore += n);
           const rotatedBack = rotateMatrixCounterClockwise(rotateMatrixCounterClockwise(rotateMatrixCounterClockwise(shifted)));
-          setField(spawnRandomTile(matrixToField(rotatedBack)));
+          const nextField = matrixToField(rotatedBack);
+          onMove(nextField);
           break;
         }
         case "ArrowRight": {
           const matrixView = buildMatrix(field());
-          const shifted = shiftMatrixRight(matrixView);
-          setField(spawnRandomTile(matrixToField(shifted)));
+          const shifted = shiftMatrixRight(matrixView, (n) => deltaScore += n);
+          const nextField = matrixToField(shifted);
+          onMove(nextField);
           break;
         }
         case "ArrowLeft": {
           const matrix = buildMatrix(field());
           const rotatedMatrixView = rotateMatrixClockwise(rotateMatrixClockwise(matrix));
-          const shifted = shiftMatrixRight(rotatedMatrixView);
+          const shifted = shiftMatrixRight(rotatedMatrixView, (n) => deltaScore += n);
           const rotatedBack = rotateMatrixCounterClockwise(rotateMatrixCounterClockwise(shifted));
-          setField(spawnRandomTile(matrixToField(rotatedBack)));
+          const nextField = matrixToField(rotatedBack);
+          onMove(nextField);
           break;
         }
       }
@@ -205,6 +248,9 @@ export default function Game(): JSXElement {
   })
 
   return <div>
+    <div class="text-center mb-5">
+      <button class="text-lg font-medium text-blue-500" onClick={reset}>Score: {score()}</button>
+    </div>
     <div class="flex justify-center mb-5">
       <button class="bg-blue-500 text-white px-3 py-2 rounded" onClick={reset}>Reset</button>
     </div>
@@ -216,23 +262,22 @@ export default function Game(): JSXElement {
           {(item) => (
             <>
               <div classList={{
-                "bg-blue-200": item === 0,
-                // "bg-blue-300 text-blue-800": item === 1,
-                "bg-blue-400 text-blue-900": item === 1,
-                "bg-indigo-300 text-indigo-800": item === 2,
-                "bg-indigo-400 text-indigo-900": item === 4,
-                "bg-violet-300 text-violet-800": item === 8,
-                "bg-violet-400 text-violet-900": item === 16,
-                "bg-purple-300 text-purple-800": item === 32,
-                "bg-purple-400 text-purple-900": item === 64,
-                "bg-fuchsia-300 text-fuchsia-800": item === 128,
-                "bg-fuchsia-400 text-fuchsia-900": item === 256,
-                "bg-pink-300 text-pink-800": item === 512,
-                "bg-pink-400 text-pink-900": item === 1024,
-                "bg-red-300 text-red-800": item === 2048,
+                "bg-blue-200": item.value === 0,
+                "bg-blue-400 text-blue-900": item.value === 1,
+                "bg-indigo-300 text-indigo-800": item.value === 2,
+                "bg-indigo-400 text-indigo-900": item.value === 4,
+                "bg-violet-300 text-violet-800": item.value === 8,
+                "bg-violet-400 text-violet-900": item.value === 16,
+                "bg-purple-300 text-purple-800": item.value === 32,
+                "bg-purple-400 text-purple-900": item.value === 64,
+                "bg-fuchsia-300 text-fuchsia-800": item.value === 128,
+                "bg-fuchsia-400 text-fuchsia-900": item.value === 256,
+                "bg-pink-300 text-pink-800": item.value === 512,
+                "bg-pink-400 text-pink-900": item.value === 1_024,
+                "bg-red-300 text-red-800": item.value === 2_048,
               }} class="group-item rounded text-2xl font-bold flex justify-center items-center w-full aspect-ratio-square">
-                <Show when={item > 0}>
-                  <span>{item}</span>
+                <Show when={item.value > 0}>
+                  <span>{item.value}</span>
                 </Show>
               </div>
             </>
@@ -240,5 +285,10 @@ export default function Game(): JSXElement {
         </For>
       </TransitionGroup>
     </div>
+    <Show when={hasWon()}>
+      <div class="text-xl text-blue-500 font-semibold text-center mt-5">
+        You have won!!!
+      </div>
+    </Show>
   </div>
 }
